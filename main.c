b5da090aa0f2f6ce481001dfa10f9181525a98f3
Iain Taylor <iain.taylor.94@gmail.com>
2023-11-04 15:29:03 +0000

    checkpoint - getVotePath() needs rewriting

--- main.c
+++ main.c
@@ -17,12 +17,14 @@ struct candidate {
 };
 
 struct treeNode {
-  int numVotes;
-  bool path[gMaxCandidates];
   struct treeNode *branches[gMaxCandidates];
+  int depth;
+  int path[gMaxCandidates];
+  int numVotes;
 };
 
 // Define Variables
+int gNumElections;
 int gNumCandidates; // Actually used
 struct candidate gCandidates [gMaxCandidates];
 
@@ -35,13 +37,18 @@ void printWinner (int); // Done
 int trailingVotes (void); // Done
 
 // Declare Functions - Manipulate tree
+bool inPath (int, struct treeNode*);
+struct treeNode* createBranch (int, struct treeNode*);
 void createTree (struct treeNode *); // Done
+void printOutput (struct treeNode *);
+void printTree (struct treeNode *);
 
 int getNumElections (void);
 int getNumCandidates (void);
 char *getCandidateName (void);
-void getVotePath (void);
-void processVote (struct treeNode *, int []);
+bool peek (FILE*);
+void getVotePath (int[]);
+void processVote (int[], struct treeNode*);
 
 void eliminateCandidate (struct treeNode *, int);
 
@@ -58,27 +65,52 @@ int main(int argc, char *argv[]) {
   else {
     fprintf (stderr, "%s opened for reading.\n" , argv[1]);
   }
-  
-  // Create candidate vote tree
-  struct treeNode *root = (struct treeNode*) calloc(1, sizeof(struct treeNode));
-  createTree (root);
+
+
+
+
 
 
   
   // Process votes - Fill tree
-  int numElections = getNumElections(); printf ("numElections = %d\n", numElections);
-  for (int i = 0; i < numElections; i++) {
-    gNumCandidates = getNumCandidates(); printf ("gNumCandidates = %d\n", gNumCandidates);
-  }
+  gNumElections = getNumElections(); printf ("numElections = %d\n", gNumElections);
+  gNumCandidates = getNumCandidates(); printf ("gNumCandidates = %d\n", gNumCandidates);
+
   for (int i = 1; i < gNumCandidates; i++) {
     gCandidates[i].name = getCandidateName();
     printf ("gCandidates[%d].name = %s\n", i, gCandidates[i].name);
   }
-//    while (true) {
-//      getVotePath();
-//      processVote ();
-//      break;
-//    }
+
+  // Create candidate vote tree
+  struct treeNode *root = (struct treeNode*) calloc(1, sizeof(struct treeNode));
+  for (int i = 0; i < gMaxCandidates; i++) {
+    root->branches[i] = NULL;
+    root->path[i] = -1;
+  }
+  root->depth = 0;
+  root->numVotes = 0;
+  printf ("root->depth = %d\n", root->depth);
+
+
+
+  
+  printf ("CREATE TREE\n");
+  createTree (root);
+
+
+  
+  printf ("PROCESS VOTE\n");
+  int votePath [gNumCandidates];
+  while (peek(gInputFile) == false) { 
+    getVotePath(votePath);
+    processVote (votePath, root);
+  }
+  
+  printf ("PRINT TREE - DEPTH FIRST\n");
+  printTree(root);
+
+
+  
 
 
   // Determine and declare winner
@@ -103,6 +135,7 @@ int main(int argc, char *argv[]) {
   
   return 0;
 }
+
 
 
 // Define Functions
@@ -134,21 +167,76 @@ int isWinner (void) {
   return leader;
 }
 
+
 void printWinner (int ID) {
   printf ("%s\n", gCandidates[ID].name);
 }
 
-void createTree (struct treeNode * tr) {
-  for (int i = 1; i < gNumCandidates; i++) {
-    if (tr->path[i] != true) {
-      tr->branches[i] = (struct treeNode*) calloc(1, sizeof(struct treeNode));
-      createTree (tr->branches[i]);
+bool inPath (int branch, struct treeNode* rt) {
+  for (int i = 0; i < rt->depth; i++) {
+    if (rt->path[i] == branch) {
+      return true;
+    }
+  }
+  return false;
+}
+
+struct treeNode* createBranch (int branch, struct treeNode* rt) {
+  struct treeNode *returnBranch = (struct treeNode*) calloc(1, sizeof(struct treeNode));
+  returnBranch->depth = rt->depth + 1;
+  for (int i = 0; i < rt->depth; i++) {
+    returnBranch->path[i] = rt->path[i];
+  }
+  returnBranch->path[rt->depth] = branch;
+  return returnBranch;
+}
+
+
+void createTree (struct treeNode * rt) {
+  for (int branch = 0; branch < gNumCandidates; branch++) {
+    if (inPath (branch, rt) == true) {
+      rt->branches[branch] = NULL;
     }
     else {
-      tr->branches[i] = NULL;
+      rt->branches[branch] = createBranch(branch, rt);
+      createTree(rt->branches[branch]);
     }
   }
 }
+    
+
+
+void printOutput (struct treeNode * rt) {
+  printf ("{ ");
+  for (int i = 0; i < rt->depth; i++) {
+    printf ("%d ", rt->path[i]);
+  }
+  printf ("} ");
+  printf ("%d\n", rt->numVotes);
+}
+
+
+void printTree (struct treeNode * rt) {
+  static int pNum = 0;
+  printf ("PT#%d: ", pNum);
+  pNum++;
+  // Print tree node - {path} votes\n
+  printOutput(rt);
+  for (int branch = 0; branch < gNumCandidates; branch++) {
+    if (rt->branches[branch] == NULL) {
+      continue;
+    }
+    else {
+      printTree(rt->branches[branch]);
+    }
+  }
+  printf ("\n");
+}
+
+
+
+
+
 
 int getNumElections (void) {
   char c = fgetc(gInputFile);
@@ -171,14 +259,58 @@ char *getCandidateName (void) {
   return name;
 }
 
+bool peek (FILE* fp) {
+  char c = fgetc(fp);
+  if (c == '\n') {
+    ungetc(c, fp);
+    return true;
+  }
+  else {
+    ungetc(c, fp);
+    return false;
+  }
+  return false;
+}
+  
+
+void getVotePath (int p[]) {
+  for (int i = 1; i < gNumCandidates; i++) {
+    fscanf (gInputFile, "%d", &p[i]);
+  }
+}
+
 int trailingVotes (void) {
   int minVotes = INT_MAX;
-  for (int i = 0; i < gNumCandidates; i++) {
+  for (int i = 1; i < gNumCandidates; i++) {
     if (!gCandidates[i].isEliminated && gCandidates[i].votes <= minVotes) {
       minVotes = gCandidates[i].votes;
     }
   }
   return minVotes;
+}
+
+
+// vp[] =  { na, 1, 2, 3 }
+// depth = {  0, 1, 2, 3 }
+void processVote (int vp[], struct treeNode *rt) {
+  printf ("PV_INIT: ");
+  if (rt->depth == 0) {
+    printf ("depth=%d: ", rt->depth);
+    printf ("PV_ROOT: nextBranch=%d\n", vp[1]); // vp[0] is empty
+    processVote (vp, rt->branches[vp[1]]);
+  }
+  else if (rt->depth  < gNumCandidates - 1) {
+    rt->numVotes++;
+    printf ("depth=%d: ", rt->depth);
+    printf ("PV_BRANCH: nextBranch=%d\n", vp[rt->depth + 1]);
+    processVote (vp, rt->branches[vp[rt->depth + 1]]);
+  }
+  else {
+    rt->numVotes++;
+    printf ("depth=%d: ", rt->depth);
+    printf ("PV_LEAF: \n");
+    return;
+  }
 }
 
 void eliminateCandidate (struct treeNode *t, int e) {
